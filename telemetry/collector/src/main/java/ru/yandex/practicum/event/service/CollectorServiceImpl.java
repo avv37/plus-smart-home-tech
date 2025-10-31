92a71744431d6edb8826aa1549267cfcae5dcc0f
avv <avv37@yandex.com>
2025-10-31 19:44:18 +0300

    доработана отправка в кафку по замечаниям

--- telemetry/collector/src/main/java/ru/yandex/practicum/event/service/CollectorServiceImpl.java
+++ telemetry/collector/src/main/java/ru/yandex/practicum/event/service/CollectorServiceImpl.java
@@ -5,6 +5,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.apache.avro.specific.SpecificRecordBase;
 import org.apache.kafka.clients.producer.Producer;
 import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.clients.producer.RecordMetadata;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import ru.yandex.practicum.event.mapper.HubEventAvroMapper;
@@ -13,6 +14,9 @@ import ru.yandex.practicum.event.model.hub.base.HubEvent;
 import ru.yandex.practicum.event.model.sensor.base.SensorEvent;
 import ru.yandex.practicum.kafka.telemetry.event.HubEventAvro;
 import ru.yandex.practicum.kafka.telemetry.event.SensorEventAvro;
+
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Future;
 
 
 @Slf4j
@@ -33,18 +37,45 @@ public class CollectorServiceImpl implements CollectorService {
     @Override
     public void sendSensorEvent(SensorEvent sensorEvent) {
         log.info("Получен SensorEvent event: {}", sensorEvent.toString());
+        String hubId = sensorEvent.getHubId();
+        Long timestamp = sensorEvent.getTimestamp().toEpochMilli();
         SensorEventAvro message = sensorEventAvroMapper.toSensorEventAvro(sensorEvent);
         log.info("SensorEventAvro message = {}", message);
-        ProducerRecord<String, SpecificRecordBase> record = new ProducerRecord<>(sensorTopic, message);
-        kafkaProducer.send(record);
+        ProducerRecord<String, SpecificRecordBase> record = new ProducerRecord<>(
+                sensorTopic,
+                null,
+                timestamp,
+                hubId,
+                message);
+        send(record, sensorEvent.getType().toString());
     }
 
     @Override
     public void sendHubEvent(HubEvent hubEvent) {
         log.info("Получен HubEventAvro event: {}", hubEvent.toString());
+        String hubId = hubEvent.getHubId();
+        Long timestamp = hubEvent.getTimestamp().toEpochMilli();
         HubEventAvro message = hubEventAvroMapper.toHubEventAvro(hubEvent);
         log.info("HubEventAvro message = {}", message);
-        ProducerRecord<String, SpecificRecordBase> record = new ProducerRecord<>(hubTopic, message);
-        kafkaProducer.send(record);
+        ProducerRecord<String, SpecificRecordBase> record = new ProducerRecord<>(
+                hubTopic,
+                null,
+                timestamp,
+                hubId,
+                message);
+        send(record, hubEvent.getType().toString());
     }
+
+    private void send(ProducerRecord<String, SpecificRecordBase> record, String type) {
+        Future<RecordMetadata> future = kafkaProducer.send(record);
+        kafkaProducer.flush();
+        try {
+            RecordMetadata metadata = future.get();
+            log.info("Событие {} было успешно сохранёно в топик {} в партицию {} со смещением {}",
+                    type, metadata.topic(), metadata.partition(), metadata.offset());
+        } catch (InterruptedException | ExecutionException e) {
+            log.warn("Не удалось записать событие {} в топик {}", type, sensorTopic, e);
+        }
+    }
+
 }
